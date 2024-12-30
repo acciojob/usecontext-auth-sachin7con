e32e3295f827276d7b908c27519606d9d35538c7
sachin7con <sachin7con@gmail.com>
2024-12-30 16:30:43 +0530

    Commit5

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-//ONS 
-import React, {useContext} from "react";
-import {AuthContext} from "./context/AuthContext"
-
-
-const Auth = () => {
-    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
-
-    return(
-        <div>
-            <h1>Click on the Checkbox to get authenticated</h1>
-            
-            {isAuthenticated ? (<p className="authText">You are now authenticated, you can proceed</p>) : (<p className="authText">you are not authenticated</p>)}
-            <input type="checkbox" checked={isAuthenticated} onChange={toggleAuth}></input>
-        </div>
-    )
-
-}
-=======
 //SGN
 import React, { useContext } from "react";
 import {AuthContext} from "./AuthProvider";
@@ -39,6 +19,5 @@ import {AuthContext} from "./AuthProvider";
         </div>
     )
 } 
->>>>>>> 408dc1d8d4793cd19b8731956b0b52a826708fe6
 
 export default Auth;
